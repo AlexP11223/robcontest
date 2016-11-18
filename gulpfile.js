6320c72f0a2565d1b5bab7bc69e59d314dea1560
Alex P <alex.pantec@gmail.com>
2016-11-18 12:00:47 +0200

    comments cleaning

--- gulpfile.js
+++ gulpfile.js
@@ -8,11 +8,8 @@ const phpunit = require('gulp-phpunit');
  |--------------------------------------------------------------------------
  | Elixir Asset Management
  |--------------------------------------------------------------------------
- |
  | Elixir provides a clean, fluent API for defining some basic Gulp tasks
- | for your Laravel application. By default, we are compiling the Sass
- | file for your application as well as publishing vendor resources.
- |
+ | for your Laravel application.
  */
 
 elixir((mix) => {
