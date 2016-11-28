80efa3f3b0ba5d71f3c4ad0978c7e10b5e4fc9f2
Alex P <alex.pantec@gmail.com>
2016-11-28 14:17:21 +0200

    main layout

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,66 +1,63 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row">
-        <div class="col-md-8 col-md-offset-2">
-            <div class="panel panel-default">
-                <div class="panel-heading">Admin login</div>
-                <div class="panel-body">
-                    <form class="form-horizontal" role="form" method="POST" action="{{ route('login') }}" id="loginForm">
-                        {{ csrf_field() }}
+<div class="col-md-8 col-md-offset-2">
+    <div class="panel panel-default">
+        <div class="panel-heading">Admin login</div>
+        <div class="panel-body">
+            <form class="form-horizontal" role="form" method="POST" action="{{ route('login') }}" id="loginForm">
+                {{ csrf_field() }}
 
-                        @if (count($errors) > 0)
-                            <div class="form-group">
-                                <div class="col-md-8 col-md-offset-4">
-                                    <ul class="error list-no-indent">
-                                        @foreach ($errors->all() as $error)
-                                            <li>{{ $error }}</li>
-                                        @endforeach
-                                    </ul>
-                                </div>
-                            </div>
-                        @endif
-
-                        <div class="form-group">
-                            <label for="name" class="col-md-4 control-label">Username</label>
-
-                            <div class="col-md-6">
-                                <input id="name" type="text" class="form-control" name="name" value="{{ old('name') }}" required autofocus>
-                            </div>
+                @if (count($errors) > 0)
+                    <div class="form-group">
+                        <div class="col-md-8 col-md-offset-4">
+                            <ul class="error list-no-indent">
+                                @foreach ($errors->all() as $error)
+                                    <li>{{ $error }}</li>
+                                @endforeach
+                            </ul>
                         </div>
+                    </div>
+                @endif
 
-                        <div class="form-group">
-                            <label for="password" class="col-md-4 control-label">Password</label>
+                <div class="form-group">
+                    <label for="name" class="col-md-4 control-label">Username</label>
 
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control" name="password" required>
-                            </div>
-                        </div>
-
-                        <div class="form-group">
-                            <div class="col-md-6 col-md-offset-4">
-                                <div class="checkbox">
-                                    <label>
-                                        <input type="checkbox" name="remember"> Remember Me
-                                    </label>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div class="form-group">
-                            <div class="col-md-8 col-md-offset-4">
-                                <button type="submit" class="btn btn-primary">
-                                    Log in
-                                </button>
-                            </div>
-                        </div>
-                    </form>
+                    <div class="col-md-6">
+                        <input id="name" type="text" class="form-control" name="name" value="{{ old('name') }}" required autofocus>
+                    </div>
                 </div>
-            </div>
+
+                <div class="form-group">
+                    <label for="password" class="col-md-4 control-label">Password</label>
+
+                    <div class="col-md-6">
+                        <input id="password" type="password" class="form-control" name="password" required>
+                    </div>
+                </div>
+
+                <div class="form-group">
+                    <div class="col-md-6 col-md-offset-4">
+                        <div class="checkbox">
+                            <label>
+                                <input type="checkbox" name="remember"> Remember Me
+                            </label>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="form-group">
+                    <div class="col-md-8 col-md-offset-4">
+                        <button type="submit" class="btn btn-primary">
+                            Log in
+                        </button>
+                    </div>
+                </div>
+            </form>
         </div>
     </div>
-
-    <script src="/js/login.js"></script>
 </div>
+
+<script src="/js/login.js"></script>
+
 @endsection
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,51 +7,86 @@
 
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', '') }}</title>
+    <title>{{ config('app.name') }}</title>
 
     <link href="/css/app.css" rel="stylesheet">
 </head>
 <body>
-    <div id="app">
-        <nav class="navbar navbar-default navbar-static-top">
-            <div class="container">
-                <div class="navbar-header">
 
-                    <a class="navbar-brand" href="{{ url('/') }}">
-                        RobLeg
+<div class="container-fluid main-container">
+
+    <div class="row">
+        <div class="col-md-8 col-md-offset-2">
+            <header class="page-header">
+                <div class="col-padding media">
+                    <a href="/" class="media-left">
+                        <img src="/img/logo.png"/>
+                    </a>
+                    <h1 class="media-body">{{ config('app.name') }}</h1>
+                </div>
+            </header>
+        </div>
+    </div>
+
+    <div class="row">
+        <section class="col-md-8 col-md-push-2">
+            <div class="col-padding">
+                <section>
+                    <script src="/js/common.js"></script>
+
+                    @yield('content')
+                </section>
+            </div>
+        </section>
+
+        <section class="col-md-2 col-md-pull-8">
+            <div class="user-menu">
+                <div class="user-menu-item">
+                    <a href="{{ url('/info') }}" role="button" class="btn btn-primary btn-block">
+                        <span class="glyphicon glyphicon-info-sign"></span>
+                        Information
+                    </a>
+                </div>
+                <div class="user-menu-item">
+                    <a href="/" role="button" class="btn btn-primary btn-block">
+                        <span class="glyphicon glyphicon-file"></span>
+                        Apply
                     </a>
                 </div>
 
                 @if (Auth::check())
-                <div>
-                    <ul class="nav navbar-nav navbar-right">
-                        <li class="dropdown">
-                            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
-                                {{ Auth::user()->name }} <span class="caret"></span>
-                            </a>
+                    @if (Auth::user()->hasRole('admin'))
+                        <div id="adminMenu">
+                            <div class="user-menu-item">
+                                <a href="/" role="button" class="btn btn-primary btn-block">
+                                    <span class="glyphicon glyphicon-check"></span>
+                                    Review teams
+                                </a>
+                            </div>
+                            <div class="user-menu-item">
+                                <a href="/" role="button" class="btn btn-primary btn-block">
+                                    <span class="glyphicon glyphicon-pencil"></span>
+                                    Add post
+                                </a>
+                            </div>
+                        </div>
+                    @endif
 
-                            <ul class="dropdown-menu" role="menu">
-                                <li>
-                                    <a href="{{ url('/logout') }}"
-                                        onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
-                                        Logout
-                                    </a>
-
-                                    <form id="logout-form" action="{{ url('/logout') }}" method="POST" style="display: none;">
-                                        {{ csrf_field() }}
-                                    </form>
-                                </li>
-                            </ul>
-                        </li>
-                    </ul>
-                </div>
+                    <div class="user-menu-item">
+                        <strong>{{ Auth::user()->name }}</strong>
+                        <div>
+                            <form action="{{ route('logout') }}" class="logout-form" method="post">
+                                {{ csrf_field() }}
+                                <button type="submit" class="btn btn-link">Logout</button>
+                            </form>
+                        </div>
+                    </div>
                 @endif
             </div>
-        </nav>
+        </section>
 
-        <script src="/js/common.js"></script>
-
-        @yield('content')
     </div>
+</div>
+
 </body>
 </html>
