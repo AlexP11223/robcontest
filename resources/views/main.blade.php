763361795f552443268442d5e8f05327132b2551
Alex P <alex.pantec@gmail.com>
2016-12-08 09:34:18 +0200

    menu on top for mobile for the main page

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -4,4 +4,18 @@
 
     @include('posts.list')
 
+    <script>
+        $(document).ready(function() {
+            //  menu on top for mobile for the main page
+
+            var menuCol = $('.user-menu').parent();
+            var contentCol = $(menuCol.parent().children()[0]);
+
+            contentCol.removeClass('col-md-push-2');
+            menuCol.removeClass('col-md-pull-8');
+
+            menuCol.prependTo(menuCol.parent());
+        });
+    </script>
+
 @endsection
