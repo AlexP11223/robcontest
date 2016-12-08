a71a0323d83a6f332af7c99bad479173ada74cac
Alex P <alex.pantec@gmail.com>
2016-12-08 09:32:37 +0200

    menu separate file

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -40,56 +40,7 @@
         </section>
 
         <section class="col-md-2 col-md-pull-8">
-            <div class="user-menu">
-                <div class="user-menu-item">
-                    <a href="{{ url('/info') }}" role="button" class="btn btn-primary btn-block">
-                        <span class="glyphicon glyphicon-info-sign"></span>
-                        Information
-                    </a>
-                </div>
-                <div class="user-menu-item">
-                    <a href="/" role="button" class="btn btn-primary btn-block">
-                        <span class="glyphicon glyphicon-file"></span>
-                        Apply
-                    </a>
-                </div>
-
-                @if (Auth::check())
-                    @if (Auth::user()->hasRole('admin'))
-                        <div id="adminMenu">
-                            <div class="user-menu-item">
-                                <a href="/" role="button" class="btn btn-primary btn-block">
-                                    <span class="glyphicon glyphicon-check"></span>
-                                    Review teams
-                                </a>
-                            </div>
-                            <div class="user-menu-item">
-                                <a href="{{ route('posts.create') }}" role="button" class="btn btn-primary btn-block">
-                                    <span class="glyphicon glyphicon-pencil"></span>
-                                    Add post
-                                </a>
-                            </div>
-                        </div>
-                    @endif
-
-                    <div class="user-menu-item">
-                        <strong>{{ Auth::user()->name }}</strong>
-
-                        <div>
-                            <a href="{{ route('change-password') }}" role="button">
-                                Change password
-                            </a>
-                        </div>
-
-                        <div>
-                            <form action="{{ route('logout') }}" class="logout-form" method="post">
-                                {{ csrf_field() }}
-                                <button type="submit" class="btn btn-link">Logout</button>
-                            </form>
-                        </div>
-                    </div>
-                @endif
-            </div>
+            @include('menu')
         </section>
 
     </div>
